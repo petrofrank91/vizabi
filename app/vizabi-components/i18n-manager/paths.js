649cf8e7d21c0c8297f1419724be8c42af426596
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-02-18 10:24:08 +0100

    Made paths’ url strings conform to sprintf-js format

--- app/vizabi-components/i18n-manager/paths.js
+++ app/vizabi-components/i18n-manager/paths.js
@@ -1,16 +1,18 @@
 define([],
     function() {
         'use strict';
+
+        // Path strings follow sprintf lib format
         var paths = [
             {
                 id: 'cms',
                 desc: 'Gapminder CMS',
-                url: 'http://stage.cms.gapminder.org/api/poFile/'
+                url: 'http://stage.cms.gapminder.org/api/poFile/%(filename)s?lang=%(lang)s'
             },
             {
                 id: 'local',
                 desc: 'Local storage',
-                url: '../../../data/i18n/'
+                url: '../../../data/i18n/%(lang)s/%(filename)s'
             }
         ];
 
