e20df0539e8db0468cb597f0c61d11f00159fd35
ambodi <amir.rahnama@gmail.com>
2014-02-20 14:10:27 +0100

    VIZ-213 - Components return their render function where applicable -- improved scale with font size and bubble radius size

--- app/widgets/chart-grid/scale.js
+++ app/widgets/chart-grid/scale.js
@@ -1,43 +1,60 @@
-define(['d3'], function (d3) {
+define(['d3'], function(d3) {
 
-	var scale = function () {
+	var scale = function() {
 
-		var xScale = null;
-		var yScale = null;
+		var xScale;
+		var yScale;
+		var bubbleSizeScale;
+		var fontSizeScale;
+
 		var domain;
 		var range;
 
-		var init = function (axis, type, domainVal, rangeVal) {
-				domain = domainVal;
-				range = rangeVal;
-				
-				if (type === "log" && axis === "x") {
-					xScale = log();
-				} 
-				else if (type === "log" && axis === "y") {
-					yScale = log();
-				}
-				else if (type === "linear" && axis === "x") {
-					xScale = linear();	
-				} 
-				else if (type === "log" && axis === "y") {
-					yScale = log();
-				}
-		};
+		var init = function(axis, type, domainVal, rangeVal) {
+			domain = domainVal;
+			range = rangeVal;
 
-		var log = function () {
-			 return d3.scale.log().domain(domain).range(range);
-		};
-
-		var linear = function () {
-			return  d3.scale.log().domain(domain).range(range);
-		};
-
-		var get = function (axis) {
-			if (axis === "x") {
-				return xScale;
+			if (type === "log" && axis === "x") {
+				xScale = log();
+			} else if (type === "log" && axis === "y") {
+				yScale = log();
+			} else if (type === "linear" && axis === "x") {
+				xScale = linear();
+			} else if (type === "log" && axis === "y") {
+				yScale = log();
+			} else if (type === "sqrt" && axis === "font") {
+				fontSizeScale = linear();
+			} else if (type === "sqrt" && axis === "bubble") {
+				bubbleSizeScale = sqrt();
 			}
-			return yScale;
+
+		};
+
+		var log = function() {
+			return d3.scale.log().domain(domain).range(range);
+		};
+
+		var linear = function() {
+			return d3.scale.log().domain(domain).range(range);
+		};
+
+		var sqrt = function() {
+			return d3.scale.sqrt().domain(domain).range(range).exponent(0.5);
+		}
+
+		var get = function(axis) {
+			var result;
+			if (axis === "x") {
+				result = xScale;
+			} else if (axis === "y") {
+				result = yScale;
+			} else if (axis === "font") {
+				result = fontSizeScale;
+			} else if (axis === "bubble") {
+				result = bubbleSizeScale;
+			}
+
+			return result;
 		};
 
 		return {
