3003a48d44c73dfbe4a680dd15efe69790ad7dd6
ambodi <amir.rahnama@gmail.com>
2014-03-08 11:08:39 +0200

    new axis breakup definition without any interdependencies
    VIZ-215 - Correctly position components using the LayoutManager

--- app/widgets/chart-grid/x-axis-grid.js
+++ app/widgets/chart-grid/x-axis-grid.js
@@ -1,17 +1,23 @@
-define(['chart-grid-x-axis'], function(xAxis) {
+define(['chart-grid-x-axis', 'chart-grid-scale'], function(xAxis, scale) {
 	var xAxisGrid = function() {
 
 		var g;
+		var axis;
+		var availableWidth = 440;
+		var availableHeight = 880;
 
 		var init = function(svg, state) {
 			g = svg.append("g");
-			xAxis.init(g, state);
+			vizState = state;
 		};
 
+		var render = function(w, h) {
+			if (axis) {
+				axis.remove();
+			}
 
-		var render = function() {
-			xAxis.render();
-			g = xAxis.setAxisGridG();
+			setAxisScale();
+			createXAxis();
 
 			return g.node().getBBox();
 		};
@@ -20,10 +26,71 @@ define(['chart-grid-x-axis'], function(xAxis) {
 			return g;
 		};
 
+		var setAxisGridG = function() {
+			axis.attr("class", "axis x grid");
+			axis.selectAll(".tick").selectAll("text").remove();
+
+			g = axis;
+		};
+
+		var setAxisScale = function() {
+			var xDomain = [];
+			if (vizState.get("minXValue") !== undefined && vizState.get("maxXValue") !== undefined) {
+				var updatedMinX = vizState.get("updatedMinXValue");
+				var minX = vizState.get("minXValue");
+
+				if (updatedMinX && updatedMinX < minX) {
+					xDomain[0] = updatedMinX;
+				} else {
+					xDomain[0] = minX;
+				}
+
+				var maxX = vizState.get("maxXValue");
+				var updatedMaxX = vizState.get("updatedMaxXValue");
+
+				if (updatedMaxX && updatedMaxX > maxX) {
+					xDomain[1] = updatedMaxX;
+				} else {
+					xDomain[1] = maxX;
+				}
+
+			} else {
+				xDomain = [vizState.getDataHelper().getMinOfXIndicator(), vizState.getDataHelper().getMaxOfXIndicator()];
+			}
+
+			// if (zoomScale) {
+			// 	xDomain[0] /= zoomScale;
+			// 	xDomain[1] /= zoomScale;
+			// }
+
+			scale.init("x", vizState.get("xAxisScale"), xDomain, [0, availableWidth]);
+		};
+
+		var createXAxis = function() {
+			var svgAxis = d3.svg.axis()
+				.scale(scale.get("x"))
+				.tickFormat(function(d) {
+					return "$" + d;
+				})
+				.ticks(10, d3.format(",d"))
+				.tickSize(-availableHeight, 0)
+				.tickPadding(5)
+				.orient("bottom");
+
+			if (vizState.get("xAxisTickValues")) {
+				svgAxis.tickValues(vizState.get("xAxisTickValues"));
+			}
+
+			axis = g
+				.attr("stroke", "lightgrey")
+				.classed("print", !vizState.get("isInteractive"))
+				.call(svgAxis);
+		};
+
 		return {
-			init: init,
 			render: render,
-			getGroup: getGroup
+			getGroup: getGroup,
+			init: init
 		};
 	};
 
--- app/widgets/chart-grid/x-axis-text.js
+++ app/widgets/chart-grid/x-axis-text.js
@@ -1,12 +1,93 @@
-define(['chart-grid-x-axis'], function(xAxis) {
+define(['chart-grid-x-axis', 'chart-grid-scale'], function(xAxis, scale) {
 	var xAxisText = function() {
 
 		var g;
+		var axis;
+		var vizState;
+		var availableWidth = 440;
+		var availableHeight = 880;
 
-		var render = function() {
-			g = xAxis.setAxisTextG();
+		var init = function(svg, state) {
+			g = svg.append("g");
+			vizState = state;
+		};
+
+		var render = function(w, h) {
+			if (w) availableWidth = w;
+			if (h) availableHeight = h;
+
+			if (axis) {
+				axis.remove();
+			}
+
+			setAxisScale();
+			createXAxis();
+			setAxisTextG();
 
 			return g.node().getBBox();
+		};
+
+		var setAxisScale = function() {
+			var xDomain = [];
+			if (vizState.get("minXValue") !== undefined && vizState.get("maxXValue") !== undefined) {
+				var updatedMinX = vizState.get("updatedMinXValue");
+				var minX = vizState.get("minXValue");
+
+				if (updatedMinX && updatedMinX < minX) {
+					xDomain[0] = updatedMinX;
+				} else {
+					xDomain[0] = minX;
+				}
+
+				var maxX = vizState.get("maxXValue");
+				var updatedMaxX = vizState.get("updatedMaxXValue");
+
+				if (updatedMaxX && updatedMaxX > maxX) {
+					xDomain[1] = updatedMaxX;
+				} else {
+					xDomain[1] = maxX;
+				}
+
+			} else {
+				xDomain = [vizState.getDataHelper().getMinOfXIndicator(), vizState.getDataHelper().getMaxOfXIndicator()];
+			}
+
+			// if (zoomScale) {
+			// 	xDomain[0] /= zoomScale;
+			// 	xDomain[1] /= zoomScale;
+			// }
+
+			scale.init("x", vizState.get("xAxisScale"), xDomain, [0, availableWidth]);
+		};
+
+		var createXAxis = function() {
+			var svgAxis = d3.svg.axis()
+				.scale(scale.get("x"))
+				.tickFormat(function(d) {
+					return "$" + d;
+				})
+				.ticks(10, d3.format(",d"))
+				.tickSize(-availableHeight, 0)
+				.tickPadding(5)
+				.orient("bottom");
+
+			if (vizState.get("xAxisTickValues")) {
+				svgAxis.tickValues(vizState.get("xAxisTickValues"));
+			}
+
+			axis = g
+				.attr("stroke", "lightgrey")
+				.classed("print", !vizState.get("isInteractive"))
+				.call(svgAxis);
+		};
+
+		var setAxisTextG = function() {
+			xAxisLabelG = d3.select(g[0][0]);
+
+			xAxisLabelG.attr("class", "axis x text");
+			xAxisLabelG.selectAll(".tick").selectAll("line").remove();
+
+			g = xAxisLabelG;
 		};
 
 		var getGroup = function() {
@@ -15,7 +96,8 @@ define(['chart-grid-x-axis'], function(xAxis) {
 
 		return {
 			render: render,
-			getGroup: getGroup
+			getGroup: getGroup,
+			init: init
 		};
 	};
 
