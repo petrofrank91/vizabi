a8932de0b151a21c3b06fd045056a06dfc1508bb
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-03-10 19:09:41 +0100

    New x axis calculation
    Breaks the axis down into axis, text and grid

--- app/widgets/chart-grid/x-axis.js
+++ app/widgets/chart-grid/x-axis.js
@@ -1,154 +1,144 @@
-define(['d3', 'chart-grid-scale'], function(d3, scale) {
+define([
+		'chart-grid-scale'
+	],
+	function(scale) {
+		function xAxis() {
+			var g;
+			
+			var axis;
+			var axisText;
+			var axisGrid;
+			
+			var vizState;
 
-	var axis = function() {
-		var vizState;
-		var g;
-		var availableWidth = 880;
-		var availableHeight = 440;
-		var axis;
-		var xAxisLabelG;
-		var xAxisTextG;
-		var xScale;
+			var availableWidth = 880;
+			var availableHeight = 500;
 
-		var init = function(_svg, state)  {
-			g = _svg;
-			vizState = state;
-		};
+			function init(svg, state) {
+				g = svg.append("g");
+				axis = svg.append('g').attr('id', 'x axis');
+				// 'stroke' in the 2 lines below should be CSS
+				axisText = svg.append('g').classed('x axistext', true).attr('stroke', 'lightgrey');
+				axisGrid = svg.append('g').classed('x axisgrid', true).attr('stroke', 'lightgrey');
+				vizState = state;
+			};
 
-		var setAxisScale = function() {
-			var xDomain = [];
-			if (vizState.get("minXValue") !== undefined && vizState.get("maxXValue") !== undefined) {
-				var updatedMinX = vizState.get("updatedMinXValue");
-				var minX = vizState.get("minXValue");
+			function render(w, h) {
+				if (w) availableWidth = w;
+				if (h) availableHeight = h;
 
-				if (updatedMinX && updatedMinX < minX) {
-					xDomain[0] = updatedMinX;
-				} else {
-					xDomain[0] = minX;
+				if (axis) {
+					axis.selectAll('g').remove();
+					axis.select('path').remove();
+					axisText.selectAll('g').remove();
+					axisGrid.selectAll('g').remove();
 				}
 
-				var maxX = vizState.get("maxXValue");
-				var updatedMaxX = vizState.get("updatedMaxXValue");
+				setXScale();
+				bakeAxis();
+			
+				breakdownAxes();
 
-				if (updatedMaxX && updatedMaxX > maxX) {
-					xDomain[1] = updatedMaxX;
+				return axis.node().getBBox();
+			};
+
+			function findXDomain() {
+				var xDomain = [];
+				if (vizState.get("minXValue") !== undefined && vizState.get("maxXValue") !== undefined) {
+					var updatedMinX = vizState.get("updatedMinXValue");
+					var minX = vizState.get("minXValue");
+
+					if (updatedMinX && updatedMinX < minX) {
+						xDomain[0] = updatedMinX;
+					} else {
+						xDomain[0] = minX;
+					}
+
+					var maxX = vizState.get("maxXValue");
+					var updatedMaxX = vizState.get("updatedMaxXValue");
+
+					if (updatedMaxX && updatedMaxX > maxX) {
+						xDomain[1] = updatedMaxX;
+					} else {
+						xDomain[1] = maxX;
+					}
+
 				} else {
-					xDomain[1] = maxX;
+					xDomain = [vizState.getDataHelper().getMinOfXIndicator(), vizState.getDataHelper().getMaxOfXIndicator()];
 				}
 
-			} else {
-				xDomain = [vizState.getDataHelper().getMinOfXIndicator(), vizState.getDataHelper().getMaxOfXIndicator()];
+				return xDomain;
+			};
+
+			function setXScale(w) {
+				scale.init("x", vizState.get("xAxisScale"), findXDomain(), [0, availableWidth]);
 			}
 
-			// if (zoomScale) {
-			// 	xDomain[0] /= zoomScale;
-			// 	xDomain[1] /= zoomScale;
-			// }
-
-			scale.init("x", vizState.get("xAxisScale"), xDomain, [0, availableWidth]);
-		};
-
-		var createXAxis = function() {
-			var svgAxis = d3.svg.axis()
-				.scale(scale.get("x"))
-				.tickFormat(function(d) {
-					return "$" + d;
-				})
-				.ticks(10, d3.format(",d"))
-				.tickSize(-availableHeight, 0)
-				.tickPadding(5)
-				.orient("bottom");
-
-			if (vizState.get("xAxisTickValues")) {
-				svgAxis.tickValues(vizState.get("xAxisTickValues"));
+			function bakeAxis() {
+				var axisMaker = d3.svg.axis()
+					.scale(scale.get("x"))
+					.tickFormat(function(d) {
+						return "$" + d;
+					})
+					.ticks(10, d3.format(",d"))
+					.tickSize(-availableHeight, 0)
+					.orient("bottom");
+				
+				if (vizState.get("xAxisTickValues")) {
+					axisMaker.tickValues(vizState.get("xAxisTickValues"));
+				}
+				
+				// stroke below should be css
+				axis.attr("stroke", "lightgrey").attr('id', 'axisNodes').call(axisMaker);
 			}
 
-			axis = g
-				.attr("stroke", "lightgrey")
-				.classed("print", !vizState.get("isInteractive"))
-				.call(svgAxis);
-		};
+			function breakdownAxes() {
+				axis.selectAll('.tick').each(function(d) {
+					axisText.node().appendChild(this.cloneNode(true));
+					axisGrid.node().appendChild(this.cloneNode(true));
+				});
 
-		var render = function(w, h) {
-			if (w) availableWidth = w;
-			if (h) availableHeight = h;
-
-			if (axis) {
-				axis.remove();
+				axisGrid.selectAll('text').remove();
+				axisText.selectAll('line').remove();
+				axis.selectAll('.tick').remove();
 			}
 
-			setAxisScale();
-			createXAxis();
+			function getGroup() {
+				return g;
+			};
+
+			function getAxisText() {
+				return axisText;
+			}
+
+			function bboxAxisText() {
+				return axisText.node().getBBox();
+			}
+
+			function getAxisGrid() {
+				return axisGrid;
+			}
+
+			function bboxAxisGrid() {
+				return axisGrid.node().getBBox();
+			}
+
+			function getAxis() {
+				return axis;
+			}
+
+			return {
+				render: render,
+				getGroup: getGroup,
+				getAxis: getAxis,
+				getAxisText: getAxisText,
+				measureAxisText: bboxAxisText,
+				getAxisGrid: getAxisGrid,
+				measureAxisGrid: bboxAxisGrid,
+				init: init
+			};
 		};
 
-		var getGroup = function() {
-			return g;
-		};
-
-		var cloneAxis = function() {
-			var node = axis.node();
-
-			var i = node.parentNode.insertBefore(node.cloneNode(true),
-				node.nextSibling);
-
-			return d3.select(i);
-		};
-
-		var setAxisTextG = function() {
-			// xAxisLabelG = d3.select(g[0][0]);
-
-			// xAxisLabelG.attr("class", "axis x text");
-			// xAxisLabelG.selectAll(".tick").selectAll("line").remove();
-
-			// return xAxisLabelG;
-
-			xAxisTextG = cloneAxis();
-
-			xAxisTextG.attr("class", "axis x text");
-			xAxisTextG.selectAll(".tick").selectAll("line").remove();
-			xAxisTextG.select('path').remove();
-
-			return xAxisTextG;
-		};
-
-		var setAxisGridG = function() {
-			// xAxisTextG = cloneAxis();
-
-			// xAxisTextG.attr("class", "axis x line");
-			// xAxisTextG.selectAll(".tick").selectAll("text").remove();
-			// xAxisTextG.select('path').remove();
-
-			// return xAxisTextG;
-
-			xAxisLabelG = d3.select(g[0][0]);
-
-			xAxisLabelG.attr("class", "axis x line");
-			xAxisLabelG.selectAll(".tick").selectAll("text").remove();
-
-			return xAxisLabelG;
-		};
-
-		var removeRestOfChartTicks = function() {
-			g.selectAll(".tick").filter(function() {
-				return d3.select(d3.select(this).node().parentNode).attr("class") === g.attr("class");
-			})
-				.remove();
-		};
-
-		var getScale = function () {
-			return xScale;
-		};
-
-		return {
-			init: init,
-			render: render,
-			getGroup: getGroup,
-			setAxisTextG: setAxisTextG,
-			setAxisGridG: setAxisGridG,
-			removeRestOfChartTicks: removeRestOfChartTicks,
-			getScale: getScale
-		};
-	};
-
-	return axis();
-});
+		return xAxis;
+	}
+);
