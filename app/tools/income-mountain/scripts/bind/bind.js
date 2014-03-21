d8cd65a112210dbe7acdb5e71e38c4a5b417ef7c
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-03-21 23:36:43 +0100

    Touch timeslider inside the Income Mountain

--- app/tools/income-mountain/scripts/bind/bind.js
+++ app/tools/income-mountain/scripts/bind/bind.js
@@ -1,8 +1,10 @@
 define([
         'd3',
-        'income-mountain/components/components'
+        'jquery',
+        'income-mountain/components/components',
+        'touchSwipe'
     ],
-    function (d3, components) {
+    function (d3, $, components) {
         'use strict';
 
         var state;
@@ -164,17 +166,99 @@ define([
                 all();
             };
 
-            //timeslider.linkState(state);
             //timeslider.setRange?
             timeslider.onplay(action);
             timeslider.onpause(action);
             timeslider.onmove(action);
         }
 
+        function bindTouchTimeslider() {
+            console.log(components)
+            var wrapper = components.get().wrapper;
+            var wrapperJq = $(wrapper.node());
+            var width = wrapperJq.width();
+
+            var speed = 0;
+
+            var lastMovement = {
+                distance: 0,
+                duration: 0,
+                direction: 'none'
+            }
+
+            wrapperJq.swipe({
+                threshold: 1,
+
+                fingers: 'all',
+
+                excludedElements: $.fn.swipe.defaults.excludedElements +
+                    ", .timeslider-1",
+
+                //Generic swipe handler for all directions
+                swipeStatus: function(event, phase, direction, distance, duration, fingers) {
+                    if (lastMovement.distance > distance) {
+                        if (direction === 'right') {
+                            direction = 'left';
+                        } else if (direction === 'left') {
+                            direction = 'right';
+                        }
+                    } else if (lastMovement.distance === distance) {
+                        direction = lastMovement.direction;
+                    }
+
+                    speed = distance / (duration - lastMovement.duration);
+
+                    if (phase === 'move') {
+                        var change = distance * speed / 200;
+
+                        // TODO: Analize if it is worthy to swap up/down to the last direction used
+                        if (direction === 'right') {
+                            var new_year = state.year + change;
+
+                            if (new_year > 2100) {
+                                new_year = 2100;
+                            }
+
+                            for (var x = state.year; x <= new_year; x++) {
+                                state.year = x;
+                                components.get().timeslider.update();
+                            }
+                        }
+                        if (direction === 'left') {
+                            var new_year = state.year - change;
+                            
+                            if (new_year < 1800) {
+                                new_year = 1800;
+                            }
+                            
+                            for (var x = state.year; x >= new_year; x--) {
+                                state.year = x;
+                                components.get().timeslider.update();
+                            }
+                        }
+                    } else if (phase === 'end') {
+                        var speedReduction = setInterval(function() {
+                            var friction = 0.001;
+                            speed -= Math.sqrt(friction * 15);
+
+                            if (speed <= 0) {
+                                clearInterval(speedReduction);
+                            }
+                        }, 200);
+                    }
+
+                    lastMovement.distance = distance;
+                    lastMovement.duration = duration;
+                    lastMovement.direction = direction;
+                }
+            });
+        }
+
         function all() {
             bindLabels();
             bindMountains();
             bindTimeslider();
+            bindTouchTimeslider();
             highlights();
         }
 
@@ -183,7 +267,8 @@ define([
             all: all,
             labels: bindLabels,
             mountains: bindMountains,
-            timeslider: bindTimeslider
+            timeslider: bindTimeslider,
+            touchTimeslider: bindTouchTimeslider
         };
     }
 );
