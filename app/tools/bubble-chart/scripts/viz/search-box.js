c7686fa8528d23c8e0aeac88af4a3013b17aed0f
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-03-22 16:20:27 +0100

    Search-box bind with picker

--- app/tools/bubble-chart/scripts/viz/search-box.js
+++ app/tools/bubble-chart/scripts/viz/search-box.js
@@ -4,17 +4,22 @@ define([], function() {
 		var g;
 		var width = 100;
 		var height = 20;
+		var picker = undefined;
 
-		var init = function(svg) {
+		var init = function(svg, geopicker) {
 			g = svg.append("g")
 				.attr("class", "search");
+			picker = geopicker;
 		};
 
 		var render = function() {
 			g.append("rect")
 				.attr("width", width)
 				.attr("height", height)
-				.attr("opacity", "0.2");
+				.attr("opacity", "0.2")
+				.on('click', function() {
+					picker.show();
+				});
 
 			return g.node().getBBox();
 		};
