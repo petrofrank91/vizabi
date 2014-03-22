ca45aa1e84b6877eb6a3fd08825a51959aa6f247
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-03-22 16:51:46 +0100

    Added magnifier and text to the search box

--- app/tools/bubble-chart/scripts/viz/search-box.js
+++ app/tools/bubble-chart/scripts/viz/search-box.js
@@ -1,25 +1,50 @@
 define([], function() {
 	var search = function() {
-
 		var g;
+		
 		var width = 100;
-		var height = 20;
+		var height = 30;
+
+		var text;
+
+		var magnifier;
+		var magnifierPath = 'm280,278a153,153 0 1,0-2,2l170,170m-91-117 110,110-26,26-110-110';
+
 		var picker = undefined;
 
-		var init = function(svg, geopicker) {
+		var init = function(svg, geopicker, t) {
 			g = svg.append("g")
-				.attr("class", "search");
+				.attr("class", "searchBox");
 			picker = geopicker;
+			text = t || '';
 		};
 
+		// from wikicommons
+		var magnifyingGlass = function() {
+			magnifier = g.append('path')
+				.attr('d', magnifierPath)
+				.attr('class', 'magnifier')
+				.attr('fill', 'none')
+				.attr('transform', 'scale(0.05,0.05)');
+		}
+
 		var render = function() {
-			g.append("rect")
-				.attr("width", width)
-				.attr("height", height)
-				.attr("opacity", "0.2")
-				.on('click', function() {
-					picker.show();
-				});
+			magnifyingGlass();
+
+			g.append('text')
+				.attr('x', 30)
+				.attr('y', 20)
+				.attr('class', 'text')
+				.text(text.toUpperCase());
+
+			g.append('rect')
+				.attr('width', 225)
+				.attr('height', 33)
+				.attr('fill', 'rgba(0,0,0,0)');
+
+			g.on('click', function() {
+				picker.show();
+			});
 
 			return g.node().getBBox();
 		};
