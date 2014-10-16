04c4d78fe652a5379f0d10c8b078de327a901f8f
ambodi <amir.rahnama@gmail.com>
2014-10-16 15:00:05 +0200

    add buttons and header to pop slider

--- src/tools/_examples/pop-slider/pop-slider.js
+++ src/tools/_examples/pop-slider/pop-slider.js
@@ -4,12 +4,16 @@ define([
 
     var lineChart = Tool.extend({
         init: function(parent, options) {
-            
+
             this.name = 'pop-slider';
             this.template = "tools/_examples/pop-slider/pop-slider";
             this.placeholder = options.placeholder;
 
             this.state = options.state;
+
+            this.addComponent('_gapminder/header', {
+                placeholder: '.vizabi-tool-title'
+            });
 
             this.addComponent('_examples/year-display', {
                 placeholder: '.vizabi-tool-year'
@@ -23,22 +27,34 @@ define([
                 placeholder: '.vizabi-tool-timeslider'
             });
 
+            this.addComponent('_gapminder/buttonlist', {
+                placeholder: '.vizabi-tool-buttonlist',
+                buttons: [{
+                            id: "geo",
+                            title: "Country",
+                            icon: "globe",
+
+                        }],
+                data: options.data
+            });
+
             this._super(parent, options);
         },
 
         getQuery: function() {
             var query = [{
-                    select: [
-                        'geo',
-                        'time',
-                        'geo.name',
-                        this.model.getState("indicator")
-                    ],
-                    where: {
-                        geo: this.model.getState("show").geo,
-                        'geo.category': this.model.getState("show")['geo.category'],
-                        time: this.model.getState("timeRange")
-                    }}];
+                select: [
+                    'geo',
+                    'time',
+                    'geo.name',
+                    this.model.getState("indicator")
+                ],
+                where: {
+                    geo: this.model.getState("show").geo,
+                    'geo.category': this.model.getState("show")['geo.category'],
+                    time: this.model.getState("timeRange")
+                }
+            }];
 
             return query;
         }
