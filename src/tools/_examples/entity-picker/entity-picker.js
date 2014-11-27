864c0b8657da608b4c271a8920f0b18415a0502e
ambodi <amir.rahnama@gmail.com>
2014-11-27 15:53:37 +0100

    Update entity_picker tool query

--- src/tools/_examples/entity-picker/entity-picker.js
+++ src/tools/_examples/entity-picker/entity-picker.js
@@ -1,6 +1,3 @@
-//FIXME: refactor hardcoded dates
-//FIXME: remove date formatting from here
-
 define([
     'lodash',
     'd3',
@@ -26,7 +23,7 @@ define([
                     component: '_gapminder/buttonlist',
                     placeholder: '.vzb-tool-buttonlist',
                     model: ['state', 'data', 'language'],
-                    buttons: ['select','more-options']
+                    buttons: ['select']
                 }];
 
                 this._super(config, options);
@@ -70,20 +67,13 @@ define([
                     dim = entities.show.dim;
 
                 var query = {};
-                query ['select'] = _.union([dim, dim + '.name', 'time', dim + '.region', model.state.indicators])
+                query ['select'] = _.union([dim, dim + '.name', dim + '.region']);
                 query['where'] = {};
                 query['where'][dim] = entities.selected;
                 query['where'][dim + '.cateogry'] = entities.show.filter[dim + '.cat'];
                 query['where'][dim + '.region'] = entities.show.filter[dim + '.region'];
-                query['where']['time'] = [time_start + "-" + time_end];
 
-                var query_all = {};
-                query_all['select'] = [dim, dim + '.name', dim + '.region', dim + '.category'];
-                query_all['where'] = {};
-                query_all['where'][dim]= ['*'];
-                query_all['where'][dim + '.category'] = ['*'];
-
-                return [query, query_all];
+                return [query];
             }
     });
 
