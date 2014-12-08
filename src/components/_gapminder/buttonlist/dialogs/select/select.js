0f0cdbe3e7a0a94ed2b25e8d4f96895bf978b775
ambodi <amir.rahnama@gmail.com>
2014-12-08 23:15:00 +0100

    Remove template code and move all the template creation to the component

--- src/components/_gapminder/buttonlist/dialogs/select/select.js
+++ src/components/_gapminder/buttonlist/dialogs/select/select.js
@@ -4,8 +4,6 @@ define([
     'jquery'
 ], function(Dialog, _, $) {
 
-    var selector;
-
     var SelectDialog = Dialog.extend({
         /**
          * Initializes the dialog component
@@ -21,50 +19,66 @@ define([
             this._super(config, parent);
         },
 
+
         postRender: function() {
-            var _this = this,
-                selector = this.element.select("vzb-entity-picker"),
-                entities_selector = d3.select(".vzb-select-list").selectAll('input');
-
-            entities_selector.on('click', function() {
-                var selected_element = d3.select(this);
-                var title = selected_element.attr('data');
-
-                if (this.checked) {
-                    d3.select('.vzb-selected-entities')
-                        .append('span')
-                        .attr('title', title)
-                        .html(title + ",");
-                } else {
-                    d3.select('.vzb-selected-entities').selectAll('span').filter(function() {
-                        return this.title === title;
-                    }).remove();
-                }
-            });
-
-            this._super();
+            this.selector = this.element.select("vzb-entity-picker");
         },
 
+        //TODO: check why update is being called multiple times
         update: function() {
             var _this = this,
                 data = this.model.data.getItems(),
                 entities = this.model.state.entities.selected;
 
-            _.each(data, function(element) {
-                _this.template_data.options.push({
-                    'id': element['geo'],
-                    'name': element['geo.name'],
-                    'selected': (entities.indexOf(element['geo']) >= 0 ? true : false)
+            var list = this.element.select('.vzb-select-list')
+                .select('ul');
+
+            var labels = list.selectAll('li')
+                .data(data)
+                .enter()
+                .append('li')
+                .append('label')
+                .on('click', function(d, i) {
+                    //_this.model.state.entities.selectEntity(this.value);
+                    var label = _this.element.select('.vzb-selected-entities'),
+                        input = d3.select(this).select('input'),
+                        span = d3.select(this).selectAll('span');
+
+                    if (input.property('checked')) {
+                        label.append('span')
+                            .attr('title', d['geo.name'])
+                            .html(d['geo.name'] + ",");
+                    } else {
+                        label.selectAll('span').filter(function() {
+                            return this.title === d['geo.name'];
+                        }).remove();
+                    }
+                })
+                .on('mouseover', function(d, i) {
+                    //_this.model.state.entities.hoverEntity(d);
+                })
+                .on('mouseover', function(d, i) {
+                    //_this.model.state.entities.hoverEntity(d);
                 });
+
+
+            labels.append('input')
+                .attr('type', 'checkbox')
+                .attr('value', function(d)  {
+                    return d['geo'];
+                })
+                .attr('name', function(d) {
+                    return d['geo.name'];
+                })
+                .attr('data', function(d) {
+                    return d['geo.name'];
+                });
+
+            labels.append('span').text(function(d) {
+                return d['geo.name'];
             });
 
-            // make sure duplicats are removed
-            this.template_data.options = _.uniq(this.template_data.options, 'name');
-
-            // TODO: better to perform this with D3
-            this.loadTemplate().then(function() {
-                _this.postRender();
-            });
+            this._super();
         }
 
     });
