53df8b1c17f79164312f0ce1a77c0ff85f9c3792
ambodi <amir.rahnama@gmail.com>
2015-01-28 15:17:57 +0100

    MultiCSV handles all queries (geo and indicator) --- Refactor needed #63

--- src/readers/multi-csv.js
+++ src/readers/multi-csv.js
@@ -35,9 +35,10 @@ define([
                     var promise = $.Deferred(),
                         row = 0,
                         q = queue(),
-                        q_data= queue(),
+                        q_data = queue(),
                         select = query.select,
-                        //TODO: Remove the hack to decide loading categories or indicators 
+                        //TODO: Remove the hack to decide loading categories or indicators
+                        //Added to bypass this: https://github.com/Gapminder/vizabi/issues/63 
                         query_type = select[1];
 
                     // for each select statement, check if theres indicator available
@@ -55,20 +56,47 @@ define([
                                                                 for (var i = query.where.time[0][0]; i <= query.where.time[0][1]; i++) {
                                                                     _this._data[order][row] = {};
                                                                     _this._data[order][row]['geo'] = datum.geo;
-                                                                    _this._data[order][row]['geo.name'] = datum['geo.name'];
-                                                                    _this._data[order][row]['geo.name'] = datum['geo.name'];
+                                                                    _this._data[order][row][query_type] = datum[query_type];
                                                                     _this._data[order][row]['time'] = i;
                                                                     row++;
                                                                 }
                                                             }
                                                         });
                                                     });
-                                                
+
                                                     callback();
                                                 });
                                         });
                                     });
-                                    
+
+                                    q_data.await(function(error, result) {
+                                        meta_callback();
+                                    });
+                                } else if (_this.isQueryIndicator(indicators, query_type)) {
+                                    _(query.where['geo.category']).forEach(function(cat) {
+                                            q_data.defer(function(callback) {
+                                                d3.csv(path + '/' + query_type + '__' + cat + '.csv',
+                                                    function(error, data) {
+                                                        _(query.where.geo).forEach(function(id) {
+                                                            console.log('id', id);
+                                                            _(data).forEach(function(datum) {
+                                                                console.log('datum',datum);
+                                                                if (datum.geo === id && datum.time >= query.where.time[0][0] && datum.time <= query.where.time[0][1]) {
+                                                                    _this._data[order][row] = {};
+                                                                    _this._data[order][row]['geo'] = datum.geo;
+                                                                    _this._data[order][row][query_type] = datum[query_type];
+                                                                    _this._data[order][row]['time'] = datum.time;
+                                                                    row++;
+
+                                                                }
+                                                            });
+                                                        });
+
+                                                        callback();
+                                                    });
+                                        });
+                                    });
+
                                     q_data.await(function(error, result) {
                                         meta_callback();
                                     });
@@ -96,6 +124,10 @@ define([
 
         getData: function() {
             return this._data;
+        },
+
+        isQueryIndicator: function(indicators, query) {
+            return _.findIndex(indicators, {id: query}) > 0;
         }
     });
 
